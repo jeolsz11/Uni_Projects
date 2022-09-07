96ec471ba4d1a11b74cc6fb6b5769111967d9430
jeolsz11 <103540507+jeolsz11@users.noreply.github.com>
2022-09-07 18:13:34 -0500

    Update and rename fetch protocol client.txt to fetch_protocol_client.c

--- C/fetch_protocol_client.c
+++ C/fetch_protocol_client.c
@@ -1,14 +1,6 @@
-/*
- * Name: Jennifer Olszyna
- * Course-Section: CS 360 01, Spring 2021
- * Assignment: 7
- * Collaborators: none
- * Resources: Dr. Jerkins
- * Description: 
- * Code starter for fetch protocol client
- * James Jerkins <jajerkins@una.edu>, March 17, 2021
- * CS360 Spring 2021
- ------------------------------------------------------------------------------*/
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Code starter for fetch protocol client
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -37,13 +29,13 @@ volatile sig_atomic_t timeout_expired;	// timer expired
 
 struct client_state
 {
-  FILE *              fd;
-  char *              filename;
-  int                 retries;  
-  enum OPCODE         op_code;  
-  unsigned short      block_id; 
-  int                 sock;     // assumtion made this is the socket
-  struct sockaddr_in  sadr;     // assumtion made this is the server address
+	FILE *              fd;
+	char *              filename;
+	int                 retries;  
+	enum OPCODE         op_code;  
+	unsigned short      block_id; 
+	int                 sock;     // assumtion made this is the socket
+	struct sockaddr_in  sadr;     // assumtion made this is the server address
 };
 
 static void send_request (struct client_state *, const char *);
@@ -53,72 +45,59 @@ static void process_datagram (struct client_state *);
 static void sig_handler (int);
 static void resend_ack (struct client_state *);
 
-int
-main (int argc, char *argv[])
-{
-  struct client_state st;
-  char *filename;
-  struct hostent *hp;
-  char *hostname;
+int main (int argc, char *argv[]) {
+	struct client_state st;
+	char *filename;
+	struct hostent *hp;
+	char *hostname;
 
-  if (argc != 3)
-    {
-      fprintf (stderr, "usage: %s hostname filename\n", getprogname ());
-      exit (EXIT_FAILURE);
-    }
+	if (argc != 3) {
+	  fprintf (stderr, "usage: %s hostname filename\n", getprogname ());
+	  exit (EXIT_FAILURE);
+	}
 
-//-------------- Code Additions Begin --------------
-    /* "get" host's name from command line */
-    if (argc == 2) 
-    {
-        hostname = argv[1];
-    } 
-    else 
-    {
-        errx(EXIT_FAILURE, "Could not find host \n");
-    }
-    
-    /* translate host name into peer's IP address */
-    hp = gethostbyname(hostname);
-    if (hp == NULL) 
-    {
-        errx(EXIT_FAILURE, "Could not translate host %s\n", hostname);
-    }
-    
-    /* setup address structure (sadr) */
-    memset(&sadr, '\0', sizeof sadr);
-    sadr.sin_family = AF_INET;
-    memcpy((char *)&sadr.sin_addr, hp->h_addr, hp->h_length);
-    sadr.sin_port = htons(SERVER_PORT);
-    
-    /* create a SOCK_DGRAM socket */
-    if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) 
-    {
-        errx(EXIT_FAILURE, "Socket creation error \n");
-    }
-//--------------  Code Additions End --------------
+	/* "get" host's name from command line */
+	if (argc == 2) {
+		hostname = argv[1];
+	} 
+	else {
+		errx(EXIT_FAILURE, "Could not find host \n");
+	}
 
-  signal (SIGALRM, sig_handler);
-  
-//--------------  Code Additions Begin --------------
-    fd = fopen(filename, "w");
-    
-    if (fd != NULL) 
-    {
-        send_request (st, filename);
-        transfer_data (st);
-        fclose(fd);
-    }
+	/* translate host name into peer's IP address */
+	hp = gethostbyname(hostname);
+	if (hp == NULL) {
+		errx(EXIT_FAILURE, "Could not translate host %s\n", hostname);
+	}
 
-   close(s);
-//-------------- Code Additions End --------------
+	/* setup address structure (sadr) */
+	memset(&sadr, '\0', sizeof sadr);
+	sadr.sin_family = AF_INET;
+	memcpy((char *)&sadr.sin_addr, hp->h_addr, hp->h_length);
+	sadr.sin_port = htons(SERVER_PORT);
 
-  return EXIT_SUCCESS;
+	/* create a SOCK_DGRAM socket */
+	if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		errx(EXIT_FAILURE, "Socket creation error \n");
+	}
+
+
+	signal (SIGALRM, sig_handler);
+
+	fd = fopen(filename, "w");
+
+	if (fd != NULL) {
+		send_request (st, filename);
+		transfer_data (st);
+		fclose(fd);
+	}
+
+	close(s);
+
+	return EXIT_SUCCESS;
 }
 
-void
-send_request (struct client_state *xferstate, const char *filename)
-{
+void send_request (struct client_state *xferstate, const char *filename) {
 //-------------- Code Additions Begin --------------
     size_t getRequest;
     int sigCheck;
@@ -136,28 +115,23 @@ send_request (struct client_state *xferstate, const char *filename)
     timeout_expired = false;
     xferstate->retries = 0;
     
-    while (!timeout_expired) 
-    {
+    while (!timeout_expired) {
     	sigCheck = select(s+1, &rfds, NULL, NULL, &tv);
     	if (sigCheck > 0) {
             recvfrom();
             alarm(0);
     	}
-    	else
-    	{
+    	else {
     	    xferstate->retries++;
     	}
     }
 //-------------- Code Additions End --------------
 }
 
-void
-transfer_data (struct client_state *xferstate)
-{
+void transfer_data (struct client_state *xferstate) {
   transfer_complete = false;
 
-  while (!transfer_complete)
-    {
+  while (!transfer_complete) {
       if (check_socket (xferstate->sock) > 0)
 	    process_datagram (xferstate);
 
@@ -170,9 +144,7 @@ transfer_data (struct client_state *xferstate)
   printf ("Done\n");
 }
 
-int
-check_socket (int fd)
-{
+int check_socket (int fd) {
   fd_set rfds;
   struct timeval tv;
 
@@ -185,14 +157,10 @@ check_socket (int fd)
   return select (fd + 1, &rfds, NULL, NULL, &tv);
 }
 
-void
-process_datagram (struct client_state *xferstate)
-{
-//-------------- Code Additions Begin --------------  
+void process_datagram (struct client_state *xferstate) {
    char buf[OPCODE_LEN + SEQNUM_LEN + BLOCKSIZE]; //buffer
    socklen_t addr_len;
    size_t packet_len;
-   
    
    alarm(0);
    timeout_expired = false;
@@ -204,44 +172,35 @@ process_datagram (struct client_state *xferstate)
    
    op_code = getOpCode(buf); 
    
-   if (op_code == OP_GET)
-   {
-        if (block_id = expected)
-        {
+   if (op_code == OP_GET) {
+        if (block_id = expected) {
             len = mkDat(buf, xferstate->block_id, buf, packet_len);
             alarm(0);
             timeout_expired = false;
             transfer_complete = true
         }
-        if (block_id = <expected)
-        {
+        if (block_id = <expected) {
             len = mkAck(&buf, xferstate->block_id);
             alarm(0);
             timeout_expired = false;
         }
-        if (block_id = >expected)
-        {
+        if (block_id = >expected) {
             len = mkErr(buf, "ERROR");
         }
         sendto (xferstate->sock, buf, len, 0,
 	                (struct sockaddr *) &xferstate->sadr, addr_len);
    }
-   if (op_code == OP_ERR)
-   {
+   if (op_code == OP_ERR) {
       transfer_complete = true
    }
 //-------------- Code Additions End --------------
 }
 
-void
-sig_handler (int sig)
-{
+void sig_handler (int sig) {
   timeout_expired = true;
 }
 
-void
-resend_ack (struct client_state *xferstate)
-{
+void resend_ack (struct client_state *xferstate) {
   /* NOTE:
    * If this function is invoked the response timeout expired.
    * The last datagram will need to be resent and restart the timer
@@ -253,16 +212,15 @@ resend_ack (struct client_state *xferstate)
   alarm (0);
   timeout_expired = false;
 
-  if (xferstate->retries < MAX_RETRIES)
-    {
+  if (xferstate->retries < MAX_RETRIES) {
       socklen_t addr_len;
       char *packet;
       size_t reply_len;
 
       if (xferstate->op_code == OP_GET)
-	reply_len = mkGet (&packet, xferstate->filename);
+		  reply_len = mkGet (&packet, xferstate->filename);
       else
-	reply_len = mkAck (&packet, xferstate->block_id);
+		  reply_len = mkAck (&packet, xferstate->block_id);
 
       addr_len = sizeof (struct sockaddr_in);
       sendto (xferstate->sock, packet, reply_len, 0,
@@ -271,8 +229,7 @@ resend_ack (struct client_state *xferstate)
       free (packet);
       alarm (TIMEOUT);
     }
-  else
-    {
+  else {
       printf ("\nTransfer timed out\n");
       transfer_complete = true;
     }
