710c2cebf185d95fae0f301073b8ff3f5fcdf0a7
jeolsz11 <103540507+jeolsz11@users.noreply.github.com>
2022-09-07 17:59:04 -0500

    Update QOTD_server.c

--- C/QOTD_server.c
+++ C/QOTD_server.c
@@ -31,9 +31,7 @@ void cleanup_exit(int);
 //'volatile' tells complier variable is NOT static like it seems to be
 volatile sig_atomic_t shut_down; //global varible
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     struct sockaddr_in server_addr;
     struct timeval tout;
     fd_set readFDS;
@@ -50,20 +48,17 @@ main(int argc, char *argv[])
     server_addr.sin_port = htons(SERVER_PORT);
 
     /* create socket s */
-    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) 
-    {
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
         errx(EXIT_FAILURE, "SOCKET CREATION ERROR \n");
     }
 
     /* bind socket s */
-    if (bind(s, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) 
-    {
+    if (bind(s, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         errx(EXIT_FAILURE, "SOCKET BIND ERROR \n");
     }
 
     /* set socket s to listen */
-    if ((listen(s, MAX_CONNECTS)) == -1) 
-    {
+    if ((listen(s, MAX_CONNECTS)) == -1) {
         errx(EXIT_FAILURE, "SOCKET LISTENING ERROR \n");
     }
 
@@ -72,8 +67,7 @@ main(int argc, char *argv[])
 
     /* have socket s wait and accept connections until shut_down;
      * shut_down is triggered by SIGTERM signal */
-    while (!shut_down) 
-    {
+    while (!shut_down)  {
         /* set read file descriptors to watch socket s to see when it has input */
         FD_ZERO(&readFDS);
         FD_SET(s, &readFDS);
@@ -85,8 +79,7 @@ main(int argc, char *argv[])
         /* check for signal every .25 seconds */
         client_s = select(s+1, &readFDS, NULL, NULL, &tout);
 
-        if (client_s == -1)
-        {
+        if (client_s == -1) {
             errx(EXIT_FAILURE, "SIGNAL ISSUE \n");
         }
 
@@ -105,9 +98,7 @@ main(int argc, char *argv[])
     return EXIT_SUCCESS;
 }
 
-void
-choose_quote (char *quote)
-{
+void choose_quote (char *quote) {
     char* quote_bank[] = {"Respect is a rational process. - Dr. McCoy",
     "Change is the essential process of all existence. - Mr. Spock",
     "Genius doesn’t work on an assembly line basis. - Capt. Kirk",
@@ -125,9 +116,7 @@ choose_quote (char *quote)
     quote = quote_bank[index];
 }
 
-void
-cleanup_exit (int sig)
-{
+void cleanup_exit (int sig) {
     //handle sigterm
     shut_down = true;
 }
